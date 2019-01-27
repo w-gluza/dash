c1236c45d7d253cffc4da444b08978a2dc96a26f
Wioletta Gluza <wiolagluza@gmail.com>
2019-01-28 00:24:14 +0100

    Updates

--- precache-manifest.8da22ed3eda2dfef2d08aca0d567d519.js
+++ precache-manifest.8da22ed3eda2dfef2d08aca0d567d519.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/dash/static/js/runtime~main.a3b4d4e2.js"
   },
   {
-    "revision": "d95d23ae1663c4321579",
-    "url": "/dash/static/js/main.d95d23ae.chunk.js"
+    "revision": "53f602d616e17d9bc4a4",
+    "url": "/dash/static/js/main.53f602d6.chunk.js"
   },
   {
     "revision": "23da9982bd35efeceb66",
     "url": "/dash/static/js/1.23da9982.chunk.js"
   },
   {
-    "revision": "d95d23ae1663c4321579",
+    "revision": "53f602d616e17d9bc4a4",
     "url": "/dash/static/css/main.fc103af4.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/dash/static/css/1.22d00426.chunk.css"
   },
   {
-    "revision": "902a7d0bf7c3b117a3a68238b180e41f",
+    "revision": "bc170a01f3790a53688a4097b5044020",
     "url": "/dash/index.html"
   }
 ];
