a0627a6b66af265f28d8f2d93503433cc8060359
Wioletta Gluza <wiolagluza@gmail.com>
2019-01-29 13:59:42 +0100

    log in for responsivness

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -11,61 +11,63 @@ class Register extends Component {
         <aside className="registration__aside">
           <Aside />
         </aside>
-        <section className="registration__form">
-          <div className="pageSwitcher__container">
-            <div className="pageSwitcher">
-              <button
-                className={
-                  this.props.loginOption
-                    ? "pageSwitcher__item pageSwitcher__item--active"
-                    : "pageSwitcher__item"
-                }
-                onClick={this.props.loginToggle}
-              >
-                Log In
-              </button>
+        <section className="registration__form__container">
+          <section className="registration__form">
+            <div className="pageSwitcher__container">
+              <div className="pageSwitcher">
+                <button
+                  className={
+                    this.props.loginOption
+                      ? "pageSwitcher__item pageSwitcher__item--active"
+                      : "pageSwitcher__item"
+                  }
+                  onClick={this.props.loginToggle}
+                >
+                  Log In
+                </button>
+                <button
+                  className={
+                    this.props.signupOption
+                      ? "pageSwitcher__item pageSwitcher__item--active"
+                      : "pageSwitcher__item"
+                  }
+                  onClick={this.props.signupToggle}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
+            <div className="registrationForm__title">
+              <span>
+                <button
+                  className={
+                    this.props.loginOption
+                      ? "registrationForm__title__link registrationForm__title__link--Active"
+                      : "registrationForm__title__link"
+                  }
+                  onClick={this.props.loginToggle}
+                >
+                  Log In
+                </button>
+                <span className="registration__form--span">or</span>
+              </span>
               <button
                 className={
                   this.props.signupOption
-                    ? "pageSwitcher__item pageSwitcher__item--active"
-                    : "pageSwitcher__item"
+                    ? "registrationForm__title__link registrationForm__title__link--Active"
+                    : "registrationForm__title__link"
                 }
                 onClick={this.props.signupToggle}
               >
                 Sign Up
               </button>
             </div>
-          </div>
-          <div className="registrationForm__title">
-            <span>
-              <button
-                className={
-                  this.props.loginOption
-                    ? "registrationForm__title__link registrationForm__title__link--Active"
-                    : "registrationForm__title__link"
-                }
-                onClick={this.props.loginToggle}
-              >
-                Log In
-              </button>
-              <span className="registration__form--span">or</span>
-            </span>
-            <button
-              className={
-                this.props.signupOption
-                  ? "registrationForm__title__link registrationForm__title__link--Active"
-                  : "registrationForm__title__link"
-              }
-              onClick={this.props.signupToggle}
-            >
-              Sign Up
-            </button>
-          </div>
-          {this.props.loginOption ? (
-            <LogIn {...this.props} />
-          ) : (
-            <SignUp {...this.props} />
-          )}
+            {this.props.loginOption ? (
+              <LogIn {...this.props} />
+            ) : (
+              <SignUp {...this.props} />
+            )}
+          </section>
         </section>
       </main>
     );
